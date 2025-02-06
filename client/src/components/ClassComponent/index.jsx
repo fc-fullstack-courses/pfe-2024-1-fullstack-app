2dcc3f00e1179b260167aaeb4bc1f66a3563cf3f
Kyrylo <fresh.js.ftd.@gmail.com>
2025-02-06 18:43:55 +0200

    Add basic props usage reminder

--- client/src/components/ClassComponent/index.jsx
+++ client/src/components/ClassComponent/index.jsx
@@ -2,10 +2,17 @@ import { Component } from 'react';
 
 class ClassComponent extends Component {
   render() {
+    const {
+      task: { title },
+    } = this.props;
+
+
+    // this.props.task.title = 'Not task name'; // Не можна мутувати свої пропси
+
     return (
       <div>
         <h2>Class component</h2>
-        <p>Text</p>
+        <p>{title}</p>
       </div>
     );
   }
