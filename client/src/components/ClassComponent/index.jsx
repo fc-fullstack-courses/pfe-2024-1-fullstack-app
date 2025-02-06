40cdc10f1ecf267ddd3fa81a50d5884945f6cce6
Kyrylo <fresh.js.ftd.@gmail.com>
2025-02-06 19:08:52 +0200

    Add basic state usage reminder

--- client/src/components/ClassComponent/index.jsx
+++ client/src/components/ClassComponent/index.jsx
@@ -1,18 +1,63 @@
 import { Component } from 'react';
 
 class ClassComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicks: 0,
+    };
+  }
+
+  handleClick = () => {
+    // alert('click');
+    // this.state.clicks += 1;
+
+    // this.setState({
+    //   clicks: this.state.clicks + 1
+    // });
+
+    // this.setState({
+    //   clicks: this.state.clicks + 1
+    // });
+
+    this.setState((state) => {
+      return {
+        clicks: state.clicks + 1,
+      };
+    });
+
+    // this.setState((state) => {
+    //   return {
+    //     clicks: state.clicks + 1,
+    //   };
+    // });
+  };
+
+  handleReset = () => {
+    this.setState({
+      clicks: 0,
+    });
+  };
+
   render() {
     const {
       task: { title },
     } = this.props;
 
+    const { clicks } = this.state;
 
     // this.props.task.title = 'Not task name'; // Не можна мутувати свої пропси
+
+    // this.state.clicks = 100;
 
     return (
       <div>
         <h2>Class component</h2>
         <p>{title}</p>
+        <p>Clicks: {this.state.clicks}</p>
+        <button onClick={this.handleClick}>Click me</button>
+        <button onClick={this.handleReset}>Reset clicks</button>
       </div>
     );
   }
--- client/src/components/FunctionalComponent/index.jsx
+++ client/src/components/FunctionalComponent/index.jsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
+
 function FunctionComponent(props) {
   const {
     task: { title },
   } = props;
 
+  // const [state, setState] = useState({clicks: 0, isAutoincrementing: false});
+
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    // setClicks(clicks + 1);
+
+    // setState({
+    //   clicks: clicks + 1
+    // })
+
+    setClicks((oldClicks) => {
+      return oldClicks + 1
+    });
+  };
+
+  const handleReset = () => {
+    setClicks(0);
+  };
+
   return (
     <div>
       <h2>Function component</h2>
       <p>{title}</p>
+      <p>Clicks: {clicks}</p>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={handleReset}>Reset clicks</button>
     </div>
   );
 }
