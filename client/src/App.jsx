8097d9ad35caf6526816ae4811efaf1efea71acb
Kyrylo <fresh.js.ftd.@gmail.com>
2025-02-07 18:39:44 +0200

    Add basic react-router usage reminder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,16 @@
-import { useState } from 'react';
-import cx from 'classnames';
-import ClassComponent from './components/ClassComponent';
-import FunctionComponent from './components/FunctionalComponent';
-import styles from './App.module.scss';
-
-const task = {
-  title: 'Learn fullstack',
-  isDone: false,
-};
+import { Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
+import Header from './components/Header';
 
 function App() {
-  const [isVisible, setIsVisible] = useState(true);
-
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
-  };
-
-  const headingClasses = cx(styles.heading, {
-    [styles.headingItalics] : !isVisible
-  });
-
   return (
     <>
-      <h1 
-      // style={{
-      //   backgroundColor: 'limegreen',
-      //   fontStyle: isVisible ? 'normal' : 'italic'
-      // }}
-      // className={`${styles.heading} ${isVisible ? '' : styles.headingItalics}`}
-      className={headingClasses}
-      >Vite app</h1>
-      <button onClick={toggleVisibility}>Toggle visibility</button>
-      {/* {isVisible && <ClassComponent prop1="test prop 1" prop2 prop3={4} prop4={{ id: 0}} task={task}/>} */}
-      {isVisible && (
-        <FunctionComponent
-          prop1='test prop 1'
-          prop2
-          prop3={4}
-          prop4={{ id: 0 }}
-          task={task}
-        />
-      )}
-
-    {isVisible ?  (
-        <FunctionComponent
-          prop1='test prop 1'
-          prop2
-          prop3={4}
-          prop4={{ id: 0 }}
-          task={task}
-        />
-      ) : <div>No component shown. Click button to reveal</div>}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </>
   );
 }
