c3690381dcf134fe3e7d9355c4815f7d8c42adb5
Kyrylo <fresh.js.ftd.@gmail.com>
2025-02-10 19:05:38 +0200

    Fix registration schema

--- client/src/validation/userValidation.js
+++ client/src/validation/userValidation.js
@@ -1,16 +1,24 @@
 import * as yup from 'yup';
 
+const USER_EMAIL_SCHEMA = yup.string().email();
+
+const USER_PASSWORD_SCHEMA = yup
+  .string()
+  .matches(
+    /^[a-zA-Z0-9]{8,32}$/,
+    'Password must contain letters on numbers and be 8 - 32 symbols long'
+  );
+
+const USER_PASSWORD_REPEAT_SCHEMA = yup
+  .string()
+  .oneOf([null, yup.ref('password')]);
+
 const USER_REGISTRATION_SCHEMA = yup.object({
   firstName: yup.string().required().min(2),
   lastName: yup.string().required().min(2),
-  email: yup.string().email().required(),
-  password: yup
-    .string()
-    .matches(
-      /^[a-zA-Z0-9]{8,32}$/,
-      'Password must contain letters on numbers and be 8 - 32 symbols long'
-    ),
-  passwordRepeat: '',
+  email: USER_EMAIL_SCHEMA.required(),
+  password: USER_PASSWORD_SCHEMA.required(),
+  passwordRepeat: USER_PASSWORD_REPEAT_SCHEMA.required(),
   gender: yup.string().required(),
 });
 
