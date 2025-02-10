866f1f6db4c38b1b3880619fea54053ffef062cf
Kyrylo <fresh.js.ftd.@gmail.com>
2025-02-10 20:52:29 +0200

    Add cors to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const rootRouter = require('./routers');
 const { basicErrorMW } = require('./middlewares/errors/basicErrorMW');
 const { PUBLIC_FOLDER_PATH } = require('./constants');
 const serverConfig = require('./configs/server.json');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static(PUBLIC_FOLDER_PATH));
 
