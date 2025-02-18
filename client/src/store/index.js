cedc93e05cd3b0e3a880ea68e523c6619b1c2067
Kyrylo <fresh.js.ftd.@gmail.com>
2025-02-18 20:36:13 +0200

    Add redux devtools to project

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,8 @@
 import { legacy_createStore as createStore } from 'redux';
 import reducer from './reducers/counterReducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 // створення стори станів редаксу
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
