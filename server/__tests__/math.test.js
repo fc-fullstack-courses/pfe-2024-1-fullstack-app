2fe5979bb8954544720607124ddb796a7b8d8a34
Kyrylo <fresh.js.ftd.@gmail.com>
2025-03-03 19:18:18 +0200

    Add some advanced matchers usage example

--- server/__tests__/math.test.js
+++ server/__tests__/math.test.js
@@ -1,11 +1,36 @@
-function sum (a, b) {
+function checkNumber (num) {
+  if(isNaN(num) || typeof num !== 'number') {
+    throw new TypeError('num is NaN value');
+  }
+
+  return num;
+}
+
+function sum(a, b) {
   return Number(a) + Number(b);
 }
 
 test('очікуємо що 2 + 2 буде 4', () => {
-  expect(sum(2,2)).toBe(4);
+  expect(sum(2, 2)).toBe(4);
 });
 
 test("очікуємо що '5' + 5 буде 10", () => {
   expect(sum('5', 5)).toBe(10);
+});
+
+test('очікуємо що 0.1 + 0.2 буде 0.3', () => {
+  expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
+});
+
+test('перевірка однаковості користувача', () => {
+
+  expect({
+    id: 1
+  }).toEqual({
+    id: 1
+  });
+});
+
+test('очікуємо що рядок апшвашф буде кидатися помилкою типу при перевірці у checkNumber', () => {
+  expect(() => checkNumber('апшвашф')).toThrow(TypeError);
 });
