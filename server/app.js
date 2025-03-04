637a1576b01ac1e72eafb5e88385127edae95725
Kyrylo <fresh.js.ftd.@gmail.com>
2025-03-04 22:13:14 +0200

    Add dockerized server example

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const rootRouter = require('./routers');
+// const rootRouter = require('./routers');
 const { basicErrorMW } = require('./middlewares/errors/basicErrorMW');
 const { PUBLIC_FOLDER_PATH } = require('./constants');
 
@@ -12,7 +12,11 @@ app.use(express.static(PUBLIC_FOLDER_PATH));
 
 app.use(express.json());
 
-app.use(rootRouter);
+// app.use(rootRouter);
+
+app.get('/test', (req, res) => {
+  res.send('You are visiting container');
+});
 
 app.use(basicErrorMW);
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const http = require('http');
-const { Server } = require('socket.io');
 const app = require('./app');
 const serverConfig = require('./configs/server.json');
-const { ChatMessage } = require('./db/models');
 
 const PORT = serverConfig.PORT;
 const HOST = serverConfig.HOST;
@@ -10,54 +8,55 @@ const HOST = serverConfig.HOST;
 const server = http.createServer(app);
 
 // io - екземпляр сервера вебсокетів
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: '*',
+//   },
+// });
 
 /*
   on - аналог addEventListener з DOM
   1 аргумент - назва події
   2 аргумент - коллбек який треба запсустити коли прийшла подія
 */
-io.on('connection', (socket) => {
-  // socket - це екземпляр з'еднання з клієнтом
-  console.log('socket connected to server');
+// io.on('connection', (socket) => {
+//   // socket - це екземпляр з'еднання з клієнтом
+//   console.log('socket connected to server');
 
-  // додаємо слухач на подію з клієнта
-  socket.on('userBtnClick', (param1, param2, param3, param4) => {
-    console.log('userBtnClick event sent from client');
+//   // додаємо слухач на подію з клієнта
+//   socket.on('userBtnClick', (param1, param2, param3, param4) => {
+//     console.log('userBtnClick event sent from client');
 
-    console.log(param1); // 10
-    console.log(param2); // 'test'
-    console.log(param3); // [true, false, false, false]
-    console.log(param4); // 'dark theme'
+//     console.log(param1); // 10
+//     console.log(param2); // 'test'
+//     console.log(param3); // [true, false, false, false]
+//     console.log(param4); // 'dark theme'
 
-    // персональна єміт для конкретного клієнта події 'userBtnClick' (аналог res.send)
-    // socket.emit('userBtnClick', false, null, 500, 'test user message');
+//     // персональна єміт для конкретного клієнта події 'userBtnClick' (аналог res.send)
+//     // socket.emit('userBtnClick', false, null, 500, 'test user message');
 
-    // єміт для всіх під'єднаних клієнтів події 'userBtnClick'
-    io.emit('userBtnClick', false, null, 500, 'test user message');
-  });
+//     // єміт для всіх під'єднаних клієнтів події 'userBtnClick'
+//     io.emit('userBtnClick', false, null, 500, 'test user message');
+//   });
 
-  socket.on('newChatMessage', async (newMessageData) => {
-    try {
-      const message = await ChatMessage.create(newMessageData);
+//   socket.on('newChatMessage', async (newMessageData) => {
+//     try {
+//       const message = await ChatMessage.create(newMessageData);
 
-      const author = await message.getAuthor();
+//       const author = await message.getAuthor();
 
-      const messageToSend = message.toJSON();
+//       const messageToSend = message.toJSON();
 
-      messageToSend.author= author;
+//       messageToSend.author= author;
 
-      io.emit('newChatMessage', messageToSend);
-    } catch (error) {
-      socket.emit('newChatMessageError', error);
-    }
-  });
-});
+//       io.emit('newChatMessage', messageToSend);
+//     } catch (error) {
+//       socket.emit('newChatMessageError', error);
+//     }
+//   });
+// });
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server started on port ${HOST}:${PORT}`);
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
